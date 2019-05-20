641781ced766fe60d1f23e40507093d535d5b8f7
PenguinLemma <thepenguinlemma@gmail.com>
2019-05-20 15:58:19 +0200

    Add shell scripts to set env variables

---
+++ set_env_var_melodic.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# To set some environment variables that are necessary to use
+# ros commands and use launch files in this project
+
+# Set the environment variables that catkin_build generates
+source devel/setup.bash
+# In case of using ROS Melodic, the following command is also necessary,
+# due to: https://github.com/ros-visualization/rviz/issues/1249
+export LC_NUMERIC="en_US.UTF-8"
+# Add path where models in the world can be found
+export GAZEBO_MODEL_PATH=$PWD/src/my_robot/worlds/model:~/.gazebo/models:$GAZEBO_MODEL_PATH
---
+++ set_env_var_pre_melodic.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# To set some environment variables that are necessary to use
+# ros commands and use launch files in this project
+
+# Set the environment variables that catkin_build generates
+source devel/setup.bash
+# Add path where models in the world can be found
+export GAZEBO_MODEL_PATH=$PWD/src/my_robot/worlds/model:~/.gazebo/models:$GAZEBO_MODEL_PATH
