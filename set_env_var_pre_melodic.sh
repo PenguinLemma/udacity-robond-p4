321530d19468002c75da89a0e5550d840af39915
PenguinLemma <thepenguinlemma@gmail.com>
2019-05-21 09:41:31 +0200

    Add env variable for location of the map

--- set_env_var_pre_melodic.sh
+++ set_env_var_pre_melodic.sh
@@ -6,3 +6,7 @@
 source devel/setup.sh
 # Add path where models in the world can be found
 export GAZEBO_MODEL_PATH=$PWD/src/my_robot/worlds/model:~/.gazebo/models:$GAZEBO_MODEL_PATH
+# Add path where map will be saved
+export MY_ROBOR_MAP_PATH=$PWD/src/my_robot/maps
+# if map folder doesn't exist, we create it
+[ -d $MY_ROBOR_MAP_PATH ] || mkdir $MY_ROBOR_MAP_PATH
